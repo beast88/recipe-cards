f7ff70a3fdad1bf176b0ca050e3cbbff131b1540
beast88 <progers999@hotmail.co.uk>
2021-05-01 12:08:06 +0100

    MongoDB connection made

--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import Landing from './pages/Landing.js';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Landing />
   </React.StrictMode>,
   document.getElementById('root')
 );
