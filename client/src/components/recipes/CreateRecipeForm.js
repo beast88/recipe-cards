f6574c88e5125725ad4efbd2976971124d6d6784
beast88 <progers999@hotmail.co.uk>
2021-06-23 15:20:50 +0100

    Styling changes to create a recipe form

--- client/src/components/recipes/CreateRecipeForm.js
+++ client/src/components/recipes/CreateRecipeForm.js
@@ -96,7 +96,7 @@ const CreateRecipeForm = (props) => {
     background = {background: 'linear-gradient(0deg, rgba(50,50,50,1) 0%, rgba(15,57,37,1) 30%, rgba(25,135,84,1) 100%)'}
   } else {
     background = {
-      background: `url("${imgPreview}") no-repeat`,
+      background: `linear-gradient(180deg, rgba(196, 196, 196, 0) 0%, rgba(0, 0, 0, 0) 65%, rgba(0, 0, 0, .85) 100%), url("${imgPreview}") no-repeat`,
       backgroundSize: "cover",
       backgroundPosition: "50%"
     }
