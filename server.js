c1ddc64b02c44af2b0a4b081556bcec0adfeb12c
beast88 <progers999@hotmail.co.uk>
2021-07-19 17:20:33 +0100

    Debugging

--- server.js
+++ server.js
@@ -29,6 +29,10 @@ app.use('/recipe', recipeRoutes)
 
 if(process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '/client/build')))
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
+  })
 }
 
 app.listen(port, () => {
